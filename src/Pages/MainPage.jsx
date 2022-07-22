68bad311b5706934cf15dfd112bdc808cc2687d2
Jamil Mohammed <zeus2tee@gmail.com>
2022-07-22 08:43:03 +0000

    first commit

---
+++ src/Pages/MainPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Helmet } from 'react-helmet'
+
+export default function MainPage() {
+  return (
+    <>
+      <Helmet>
+        <title>Charity Village Ghana</title>
+      </Helmet>
+    
+      <Container></Container>
+    </>
+  );
+};
